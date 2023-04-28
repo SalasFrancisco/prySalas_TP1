0ce11355ac27767586651c9928d430c2ad7816cd
Spanqy <Alumnos@PC190>
2023-04-28 12:14:54 -0300

    cambi28/04/23_12.15

---
+++ pryMiPrimerApp/frmPrincipal.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace pryMiPrimerApp
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mrcAplicaciones = new System.Windows.Forms.GroupBox();
+            this.cmdAlacena = new System.Windows.Forms.Button();
+            this.cmdAdministradorDeGastos = new System.Windows.Forms.Button();
+            this.mrcAplicaciones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mrcAplicaciones
+            // 
+            this.mrcAplicaciones.Controls.Add(this.cmdAdministradorDeGastos);
+            this.mrcAplicaciones.Controls.Add(this.cmdAlacena);
+            this.mrcAplicaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mrcAplicaciones.Location = new System.Drawing.Point(16, 16);
+            this.mrcAplicaciones.Name = "mrcAplicaciones";
+            this.mrcAplicaciones.Size = new System.Drawing.Size(217, 273);
+            this.mrcAplicaciones.TabIndex = 0;
+            this.mrcAplicaciones.TabStop = false;
+            this.mrcAplicaciones.Text = "Aplicaciones";
+            // 
+            // cmdAlacena
+            // 
+            this.cmdAlacena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAlacena.Location = new System.Drawing.Point(22, 47);
+            this.cmdAlacena.Name = "cmdAlacena";
+            this.cmdAlacena.Size = new System.Drawing.Size(170, 88);
+            this.cmdAlacena.TabIndex = 0;
+            this.cmdAlacena.Text = "Alacena";
+            this.cmdAlacena.UseVisualStyleBackColor = true;
+            this.cmdAlacena.Click += new System.EventHandler(this.cmdAlacena_Click);
+            // 
+            // cmdAdministradorDeGastos
+            // 
+            this.cmdAdministradorDeGastos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAdministradorDeGastos.Location = new System.Drawing.Point(22, 153);
+            this.cmdAdministradorDeGastos.Name = "cmdAdministradorDeGastos";
+            this.cmdAdministradorDeGastos.Size = new System.Drawing.Size(170, 88);
+            this.cmdAdministradorDeGastos.TabIndex = 1;
+            this.cmdAdministradorDeGastos.Text = "Administrador de Gastos";
+            this.cmdAdministradorDeGastos.UseVisualStyleBackColor = true;
+            this.cmdAdministradorDeGastos.Click += new System.EventHandler(this.cmdAdministradorDeGastos_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 301);
+            this.Controls.Add(this.mrcAplicaciones);
+            this.Name = "frmPrincipal";
+            this.Text = "Aplicaciones";
+            this.mrcAplicaciones.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mrcAplicaciones;
+        private System.Windows.Forms.Button cmdAdministradorDeGastos;
+        private System.Windows.Forms.Button cmdAlacena;
+    }
+}
---
+++ pryMiPrimerApp/frmPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryMiPrimerApp
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdAlacena_Click(object sender, EventArgs e)
+        {
+            frmAlacena ventanaAlacena = new frmAlacena();
+
+            ventanaAlacena.ShowDialog();
+        }   
+
+        private void cmdAdministradorDeGastos_Click(object sender, EventArgs e)
+        {
+            Form1 ventanaControlDeGastosMensuales = new Form1();
+
+            ventanaControlDeGastosMensuales.ShowDialog();
+        }
+    }
+}
